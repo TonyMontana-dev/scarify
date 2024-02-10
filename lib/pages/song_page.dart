113b064eab10283ec6a23b5ef3c65f55b1f7eb66
marcelliA <72955058+TonyMontana-dev@users.noreply.github.com>
2024-02-10 15:37:44 -0600

    Uploading few changes

--- lib/pages/song_page.dart
+++ lib/pages/song_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:scarify/components/neu_box.dart';
 
 class SongPage extends StatelessWidget {
   const SongPage({super.key});
@@ -6,7 +7,18 @@ class SongPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Song Page")),
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(
+        title: Text("Song Page"),
+      ),
+      body: Center(
+        child: NeuBox(
+          child: Icon(
+            Icons.search
+            size: 100,
+            ),
+          ),
+        ),
     );
   }
 }
